420f91163fda5bf8d114fd85c13288e40bc57093
ManviKoul1 <99130254+ManviKoul1@users.noreply.github.com>
2022-03-25 14:06:26 +0530

    Update Q4.c

--- Q4.c
+++ Q4.c
@@ -1,3 +1,5 @@
+//4. Write a program where pthread task displays the thread id and also prints the calling process pid.
+
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/syscall.h>
@@ -5,3 +7,4 @@
  
 
  pid_t x = syscall(__NR_gettid);
+//to be completed
